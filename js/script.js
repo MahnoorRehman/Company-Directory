a30b64de8011f99a11138b05bd5d17dbe7f23e07
MahnoorRehman <mahnoorrehman061@gmail.com>
2023-04-01 03:52:35 +0100

    Search Modal Done

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
+
+
 $(document).ready(function () {
+
     $("#btn-search").click(function () {
-        console.log('i am clicked')
         $("#searchModal").show();
     });
 
-    $(".close").click(function () {
+    $("#btn-close").click(function () {
         $("#searchModal").hide();
-    });
+    })
+
 });
