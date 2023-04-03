4c3a90e36881ad4bad200eb031be58340b2d3f57
MahnoorRehman <mahnoorrehman061@gmail.com>
2023-04-03 02:44:16 +0100

    update php script to get protect from sql injection

--- php/getAll.php
+++ php/getAll.php
@@ -14,26 +14,39 @@ $query='SELECT p.firstName, p.lastName, p.jobTitle, p.email, d.name as departmen
         LEFT JOIN location l ON(l.ID=d.locationID) 
         ORDER BY p.lastName, p.firstName, d.name, l.name';
 
-$result=mysqli_query($con, $query);
 
-if($result===false){
+$stmt= mysqli_prepare($con, $query);
+$info=[];
+if($stmt){
+    mysqli_stmt_execute($stmt);
+    $result= mysqli_stmt_get_result($stmt);
+   
+    if($result===false){
+
+        $data=[
+            'success'=> false,
+            'message'=> 'Sorry No data',
+            'data'=>$info
+
+        ];
   
-   mysqli_close($con);
-   $info=[];
+        mysqli_close($con);   
+        echo json_encode($data);
+     
+     } else{
+         while($row=mysqli_fetch_assoc($result)){
+            array_push($info, $row);
+         }
 
-   echo json_encode($info);
-
-} else{
-    $info=[];
-
-    while($row=mysqli_fetch_assoc($result)){
-       array_push($info, $row);
-    }
-    mysqli_close($con);
-
-    echo json_encode($info);
-}
-
+         $data=[
+            'success'=> true,
+            'message'=> 'Data Fetch Successful',
+            'data'=>$info
+         ];
+         mysqli_close($con);
+         echo json_encode($info);
+     }
+} 
 
 
 
